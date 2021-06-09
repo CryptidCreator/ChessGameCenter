3d75694b04124a316a5caf172b906d401bb607f3
Malik FEUGUEUR <malikfeugueur@gmail.com>
2021-06-09 16:38:17 +0200

    Update 09/06

--- ScriptV1.py
+++ ScriptV1.py
@@ -1,31 +1,33 @@
-import time
 import datetime
 
 
 class Player:
 
-    def __init__(self, family_name, firstname, date_of_birth, gender, ranking):
+    def __init__(self, family_name, firstname, date_of_birth, gender, points):
         self.family_name = family_name
         self.firstname = firstname
         self.date_of_birth = date_of_birth
         self.gender = gender
-        self.ranking = ranking
+        self.points = points
 
 
-class tournament:
+class Tournament:
 
-    def __init__(self, name, place, date, tour, participants, time_control, description):
+    def __init__(self, name, place, date, participants, time_control, description):
         self.name = name
         self.place = place
         self.date = date
-        self.rounds_number = 0
-        self.tour = tour
+        self.round_number = 0   # Pas modifiable pendant la création
+        self.rounds = []        # Pas modifiable pendant la création
         self.participants = participants
         self.time_control = time_control
         self.description = description
 
 
-# Tour en objet avec match en attribut
+class Round:
+
+    def __init__(self, matches):
+        self.matches = matches
 
 
 def set_player():
@@ -43,24 +45,23 @@ def set_player():
         if ask_gender == "M" or ask_gender == "F":
             gender = ask_gender
             break
-    ranking = 0
-    player_Add = Player(family_name, firstname, date_of_birth, gender, ranking)
+    points = 0
+    player_Add = Player(family_name, firstname, date_of_birth, gender, points)
     return player_Add
 
 
 def set_tournament():
     participants = []
-    name = input("Nom:")
-    place = input("Lieu :")
+    name_set = input("Nom:")
+    place_set = input("Lieu :")
     while True:
         try:
-            date = datetime.datetime.strptime(input("Date (Jour Mois Année) :"), '%d %m %Y')
+            date_set = datetime.datetime.strptime(input("Date (Jour Mois Année) :"), '%d %m %Y')
         except ValueError:
             print("Vous devez rentrer une date valide. Ex : 14 07 1789")
         break
-    tour = 0
     display_players(players)
-    ask_indices = input("Joueur participant (Format : 1,2,3 ; Max. 8) :")
+    ask_indices = input("Joueur participant (Format : 1,2,3,4 [nbre pair] ; Max. 8) :")
     indices = [int(n) for n in ask_indices.split(sep=",") if n.isdigit()]
     print(indices)
     for i in range(len(indices)):
@@ -68,17 +69,21 @@ def set_tournament():
             participants.append(players[indices[i] - 1])
     display_players(participants)
     print("Controle du temps (Bullet = 1; Blitz = 2; Coup Rapide = 3) :")
-    ask_mode = input("Choississez un des trois modes :")    # While pour conditions
-    if ask_mode == 1:
-        time_control = "Bullet"
-    elif ask_mode == 2:
-        time_control = "Blitz"
-    elif ask_mode == 3:
-        time_control = "Coup rapide"
-    else:
-        print("Choississez 1, 2 ou 3")
+    while True:
+        ask_mode = int(input("Choississez un des trois modes :"))
+        if ask_mode == 1:
+            time_control_set = "Bullet"
+            break
+        elif ask_mode == 2:
+            time_control_set = "Blitz"
+            break
+        elif ask_mode == 3:
+            time_control_set = "Coup rapide"
+            break
+        else:
+            print("Choississez 1, 2 ou 3")
 #    description = input("Description :")
-    tournament_Add = tournament(name, place, date, tour, participants, time_control, "Tkt bg")
+    tournament_Add = Tournament(name_set, place_set, date_set, participants, time_control_set, "Tkt bg")
     return tournament_Add
 
 
@@ -87,7 +92,7 @@ def display_player(var):
     print("Prenom :", var.firstname)
     print("Date de naissance :", var.date_of_birth)
     print("Sexe :", var.gender)
-    print("Classement :", var.ranking)
+    print("Points :", var.points)
 
 
 def display_players(list):
@@ -100,26 +105,89 @@ def display_tournament(contest):
     print("Nom :", contest.name)
     print("Lieu :", contest.place)
     print("Date :", contest.date)
-    print("Nombre de tours :", contest.rounds_number)
-    print("Tournees :", contest.tour)
+    print("Nombre de tours :", contest.round_number)
+    print("Tournees :", contest.rounds)
     print("Participants :")
     display_players(contest.participants)
     print("Contrôle du temps :", contest.time_control)
     print("Description : ", contest.description)
 
-# modélisation match
+
+def display_match(tupl):
+    print("Match :")
+    for i in range(len(tupl)):
+        temp = tupl[i]
+        print("Joueur : ")
+        print(temp.family_name, temp.firstname)
+
+
+def ask_result(tupl):
+    while True:
+        print("Premier joueur gagnant : 1")
+        print("Deuxième joueur gagnant : 2")
+        print("Égalité : 3")
+        ask_r = int(input("Quel est le résultat du match? :"))
+        if ask_r == 1:
+            temp = tupl[0]
+            temp.points += 1
+            break
+        elif ask_r == 2:
+            temp = tupl[1]
+            temp.points += 1
+            break
+        elif ask_r == 3:
+            for i in range(2):
+                temp = tupl[i]
+                temp.points += 1/2
+            break
+        else:
+            print("Choississez 1, 2 ou 3")
+
+
+def points_update(var):
+    sorted_list = [""] * len(var)
+    for j in range(len(var)):
+        for i in range(len(var)):
+            short = 0
+            temp_i = var[i]
+            if short < temp_i.points:   # AttributeError: 'int' object has no attribute 'points'
+                short = temp_i.points
+                sorted_list[j] = var[i]
+                var[i] = 0
+    var = sorted_list
+    return var
+
+
+def play_round(competition):
+    playing_matches = []
+    done_matches = []
+    points_update(competition.participants)
+    half = int(len(competition.participants)/2)
+    for j in range(half):
+        duo = (competition.participants[j], competition.participants[j + int(half)])
+        playing_matches.append(duo)
+        display_match(duo)
+    for duo in playing_matches:
+        result = ask_result(duo)
+        match = duo, result
+        done_matches.append(match)
 
 
 player1 = Player("Feugueur", "Malik", "2000-03-13", "M", 0)
-player2 = Player("Bulard", "Julien", "1992-05-27", "M", 1)
-player3 = Player("Maukeur", "Feli", "1999-09-09", "F", 2)
-players = [player1, player2, player3]
+player2 = Player("Bulard", "Julien", "1992-05-27", "M", 0)
+player3 = Player("Maukeur", "Feli", "1999-09-09", "F", 0)
+player4 = Player("Juste", "Innome", "1990-11-06", "M", 0)
+players = [player1, player2, player3, player4]
+
+tournament = Tournament("CG-Senlis", "Senlis", "2021-06-02", players, "Bullet", "Tkt bg")
+
 while True:
     print("MENU :")
     print("- CREER LES JOUEURS : 1")
     print("- AFFICHER LES JOUEURS : 2")
     print("- CREER LE TOURNOI : 3")
     print("- AFFICHER LE TOURNOI : 4")
+    print("- LANCER LE TOURNOI : 5")
     print("- QUITTER L'APPLICATION : 0")
     Chiffre = int(input("CHOIX :"))
     if Chiffre == 1:
@@ -134,6 +202,8 @@ while True:
         print("")
         display_tournament(tournament)
         print("")
+    elif Chiffre == 5:
+        play_round(tournament)
     elif Chiffre == 0:
         break
     else:
@@ -141,11 +211,9 @@ while True:
         print("Veuillez choisir un chiffre disponible!")
         print("")
 
-# Git init puis git remote add
 
 # Aprés lancement du tournoi :
-# Generer les matchs et ajouter round (Objets)
-# Faire tour et match pour un round. Afficher match et demander les résultat
+# Faire round et match pour un round. Afficher match et demander les résultat
 # Boucle
 # Donner les resultats
 # calculer le classement
